6b9d275436119ef2a1cd7c341206eef2d77411c4
noah <noahlog@naver.com>
2019-03-07 20:56:34 +0900

    Initial commit

---
+++ src/Festival.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Festival {
+
+    public static void main(String args[]){
+
+        int c ; //테스트 케이스 ( <= 100 )
+        int n ; //공연장을 대여할 수 있는 날들의 수 ( 1 <= n <= 100)
+        int l ; //공연 팀의 수 ( 1 <= ㅣ <= 100)
+
+        //입력 받는 버퍼 리더 ( .read() / .readLine() )
+        BufferedReader bufferedReader = new BufferedReader((new InputStreamReader(System.in)));
+
+
+
+
+
+    }
+
+}
